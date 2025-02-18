cffb0ddfea5945bb5264656149cedf55fa031b38
JoanDMH <90460990+JoanDMH@users.noreply.github.com>
2025-02-18 11:29:40 -0500

    Update Persona.java

--- src/com/educationallab/console/Persona.java
+++ src/com/educationallab/console/Persona.java
@@ -1,15 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.educationallab.console;
 
 import java.io.Serializable;
 
-/**
- *
- * @author Estudiante_MCA-Joan Martinez
- */
 public class Persona implements Serializable {
 
     private  static  final  long serialVersionUID = 1L;
