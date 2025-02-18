087a03d500012c55869a26e16632d86c60421782
rediah10k <hjarango@unillanos.edu.co>
2025-02-18 09:30:29 -0500

    Integracion de cambios de github

---
+++ src/com/educationallab/console/Estudiante.java
@@ -0,0 +1,31 @@
+
+
+package com.educationallab.console;
+
+
+
+/**
+ *
+ * @author Estudiante_MCA
+ */
+public class Estudiante extends Persona  {
+
+    private  static  final  long serialVersionUID = 1L;
+    private String codigo;
+    private String programa;
+    private Boolean activo;
+    private Double promedio;
+    public Estudiante(Integer id, String nombres, String apellidos, String email, String codigo, String programa, Boolean activo, Double promedio){
+        super(id, nombres, apellidos, email);
+        this.codigo = codigo;
+        this.programa = programa;
+        this.activo = activo;
+        this.promedio = promedio;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante{" + super.toString() + "codigo=" + codigo + ", programa=" + programa + ", activo="+activo+", promedio="+promedio+ '}';
+    }
+
+}
---
+++ src/com/educationallab/console/Persona.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.educationallab.console;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Estudiante_MCA-Joan Martinez
+ */
+public class Persona implements Serializable {
+
+    private  static  final  long serialVersionUID = 1L;
+    private Integer id;
+    private String nombres;
+    private String apellidos;
+    private String email;
+
+
+    public Persona(Integer id, String nombres, String apellidos, String email){
+        this.id = id;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.email = email;
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+
+
+    @Override
+    public String toString(){
+        return "Persona(id=" + this.id + ",nombres=" + this.nombres + ",apellidos=" + this.apellidos + "," + this.email + ")";
+    }
+
+
+}
