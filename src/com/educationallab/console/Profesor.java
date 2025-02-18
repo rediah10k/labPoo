5c968a0ec7eaf211256f06e5905755af15623695
JoanDMH <90460990+JoanDMH@users.noreply.github.com>
2025-02-18 09:49:28 -0500

    Create Profesor.java

---
+++ src/com/educationallab/console/Profesor.java
@@ -0,0 +1,27 @@
+
+package com.educationallab.console
+
+
+/**
+ *
+ * @author Estudiante_MCA Joan Martinez y Johan Arango
+ */
+
+public class Profesor extends Persona {
+    private  static  final  long serialVersionUID = 1L;
+    private String tipoContrato;
+   
+  
+    public Empleado(Integer id, String nombres, String apellidos, String email, String tipoContrato){
+        super(id, nombres, apellidos, email);
+        this.tipoContrato = tipoContrato;
+        
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado("+ super.toString() + ", tipoContrato =" + tipoContrato)";
+    }
+    
+    
+}
