ad7075ed88d4d6df8768d22109dec96fb366253b
JoanDMH <90460990+JoanDMH@users.noreply.github.com>
2025-02-18 11:32:09 -0500

    Update Servicios.java

--- src/com/educationallab/console/Servicios.java
+++ src/com/educationallab/console/Servicios.java
@@ -1,8 +1,5 @@
 package com.educationallab.console
 
-/**
- * @author Estudiante_MCA Joan Martinez y Johan Arango
- */
 public interface Servicios {
     String imprimirPosicion(Integer posicion);
     Integer cantidadActual();
