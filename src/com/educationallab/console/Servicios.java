fb3dd5af67bafd56af37fe9c12ff2a35ed4f2bd4
JoanDMH <90460990+JoanDMH@users.noreply.github.com>
2025-02-18 09:41:57 -0500

    Create Servicios.java

---
+++ src/com/educationallab/console/Servicios.java
@@ -0,0 +1,10 @@
+package com.educationallab.console
+
+/**
+ * @author Estudiante_MCA Joan Martinez y Johan Arango
+ */
+public interface Servicios {
+    String imprimirPosicion(Integer posicion);
+    Integer cantidadActual();
+    List<String> imprimirListado();
+} 
